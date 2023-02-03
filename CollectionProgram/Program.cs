909441e6d34e2e49184988b8ace3113ec51c85b1
Nepolean23 <nlean1996@gmail.com>
2023-02-03 21:20:16 +0530

    [NEPOLEAN]-Code for Collections Programs

---
+++ CollectionProgram/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CollectionsPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
